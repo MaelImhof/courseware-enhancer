35d1a26051c8c602c74b87b87bcc3adc3135e721
LasymAndIris <mael.imhof@protonmail.com>
2022-11-11 13:45:53 +0100

    Fixed #2
    The current playback rate should now be displayed at any time in the normal place for it to be shown in

--- courseware-enhancer.js
+++ courseware-enhancer.js
@@ -9,13 +9,25 @@
 class CoursewareEnhancer {
 
     /**
+     * The maximum playback rate you can use on a video element in most browsers.
+     * May not be exact, but anyway, who seriously watches a video in 16x speed?
+     */
+    static MAX_PLAYBACK_RATE = 16;
+
+    /**
+     * The minimum playback rate you can use on a video element, defined arbitrarily
+     * by the fact that the step is 0.25 and a speed of 0 makes no sense
+     */
+    static MIN_PLAYBACK_RATE = 0.25;
+
+    /**
      * In some scenarios, this extension will increase or decrease the speed
      * of a playing video each time the user releases a key
      * 
      * This variable indicates how much the playback rate will be increased
      * or decreased at each press
      */
-    speedStep = 0.25;
+    static speedStep = 0.25;
 
     /**
      * In some scenarios, this extension will make the playing video go
@@ -24,7 +36,7 @@ class CoursewareEnhancer {
      * This variable indicates how many seconds should be skipped or
      * backwarded at each press of the corresponding button
      */
-    timeStep = 5;
+    static timeStep = 5;
 
     /**
      * Initializes the static Courseware Enhancer by registering the
@@ -36,6 +48,7 @@ class CoursewareEnhancer {
     static init() {
         
         // Setting the constant step values
+        CoursewareEnhancer.MAX_PLAYBACK_RATE = 16;
         CoursewareEnhancer.speedStep = 0.25;
         CoursewareEnhancer.timeStep = 5;
 
@@ -99,10 +112,25 @@ class CoursewareEnhancer {
      *                          playback rate of the videos (can be negative)
      */
     static addToSpeed(addition) {
+
+        // Set the play back rate for any video on the page
         let videos = document.getElementsByTagName('video');
         for (let i = 0; i < videos.length; ++i) {
+
+            // If the video is already at maximum speed, don't add up
+            if (addition > 0 && videos[i].playbackRate >= CoursewareEnhancer.MAX_PLAYBACK_RATE)
+                continue;
+
+            // If the video is already at minimum speed, don't add down
+            else if (addition < 0 && videos[i].playbackRate <= CoursewareEnhancer.MIN_PLAYBACK_RATE)
+                continue;
+            
             videos[i].playbackRate += addition;
         }
+
+        // Display the current value of the play back rate
+        let currentPlayBackRate = document.getElementsByTagName('video')[0].playbackRate;
+        document.querySelector('button.control.speed-button>span.value').innerText = currentPlayBackRate + "x";
     }
 }
 
