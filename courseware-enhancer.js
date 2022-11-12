4f2475961c1551b60fcf3e84b7aa7f6da7e8ce72
LasymAndIris <mael.imhof@protonmail.com>
2022-11-12 11:16:13 +0100

    Fixed #5 and added constants
    Pressing the space bar without the video selected should now not result in a scrolling anymore.
    
    Plus, the script seems now more readable to me with named constants instead of random values. These constants will hopefully be used in the future to make the users able to decide what key to use for what action

--- courseware-enhancer.js
+++ courseware-enhancer.js
@@ -21,6 +21,54 @@ class CoursewareEnhancer {
     static MIN_PLAYBACK_RATE = 0.25;
 
     /**
+     * The key value used to determine if the script should slow
+     * down the video
+     * 
+     * By default, the down arrow is used to slow down the video
+     */
+    static SlowDownKey = 'ArrowDown';
+
+    /**
+     * The key value used to determine if the script should speed
+     * up the video
+     * 
+     * By default, the up arrow is used to speed up the video
+     */
+    static SpeedUpKey = 'ArrowUp';
+
+    /**
+     * The key value used to determine if the script should make
+     * the video go forward to skip the next seconds
+     * 
+     * By default, the right arrow is used to forward the video
+     */
+    static ForwardKey = 'ArrowRight';
+
+    /**
+     * The key value used to determine if the script should make
+     * the video go backward to see the last seconds again
+     * 
+     * By default, the left arrow is used to backward the video
+     */
+    static BackwardKey = 'ArrowLeft';
+
+    /**
+     * The key value used to determine if the script should
+     * pause/resume the video
+     * 
+     * By default, the space bar is used to pause/resume the video
+     */
+    static PauseResumeKey = ' ';
+
+    /**
+     * The key value used to determine if the script should
+     * toggle the fullscreen mode of the video
+     * 
+     * By default, the f key is used to toggle fullscreen mode
+     */
+    static FullScreenKey = 'f';
+
+    /**
      * In some scenarios, this extension will increase or decrease the speed
      * of a playing video each time the user releases a key
      * 
@@ -56,23 +104,24 @@ class CoursewareEnhancer {
         window.addEventListener('keyup', (event) => {
 
             /**
-             * Whenever the up or down keys are pressed, speed up
-             * or slow down the currently playing video
+             * Whenever the defined keys are pressed, the script
+             * will either speed up or slow down the currently
+             * playing video
              */
-            if (event.key == 'Down' || event.key == 'ArrowDown') {
+            if (event.key == CoursewareEnhancer.SlowDownKey) {
                 event.preventDefault();
                 CoursewareEnhancer.addToSpeed( - CoursewareEnhancer.speedStep );
             }
-            else if (event.key == 'Up' || event.key == 'ArrowUp') {
+            else if (event.key == CoursewareEnhancer.SpeedUpKey) {
                 event.preventDefault();
                 CoursewareEnhancer.addToSpeed( CoursewareEnhancer.speedStep );
             }
 
             /**
-             * Whenever the space bar is released, pause or resume the
-             * current video
+             * If the pause/resume key has been pressed, toggle
+             * the playing state of the video
              */
-            else if (event.key == 'Space' || event.key == ' ') {
+            else if (event.key == CoursewareEnhancer.PauseResumeKey) {
                 event.preventDefault();
                 let video = document.getElementsByTagName('video')[0];
                 if (video.paused) video.play();
@@ -80,26 +129,47 @@ class CoursewareEnhancer {
             }
 
             /**
-             * Whenever the right or the left arrows are pressed, forward
-             * or backward the video with the step defined in the class
+             * Go backward or forward in the video whenever the user
+             * requests it (that's to say when the right keys are pressed)
              */
-            else if (event.key == 'Right' || event.key == 'ArrowRight') {
+            else if (event.key == CoursewareEnhancer.ForwardKey) {
                 event.preventDefault();
                 let video = document.getElementsByTagName('video')[0];
                 video.currentTime += CoursewareEnhancer.timeStep;
             }
-            else if (event.key == 'Left' || event.key == 'ArrowLeft') {
+            else if (event.key == CoursewareEnhancer.BackwardKey) {
                 event.preventDefault();
                 let video = document.getElementsByTagName('video')[0];
                 video.currentTime -= (video.currentTime >= CoursewareEnhancer.timeStep ? CoursewareEnhancer.timeStep : video.currentTime);
             }
 
             /**
-             * Whenever the "f" key is pressed, toggle the full screen mode
-             * of the video
+             * Toggle the fullscreen mode of the video by simply pressing a
+             * key on the keyboard rather than having to use the mouse
              */
-            else if (event.key == "f") {
+            else if (event.key == CoursewareEnhancer.FullScreenKey) {
                 document.querySelector('.control.add-fullscreen').click();
+            }
+        });
+
+        /**
+         * Fixes issue #5
+         * https://github.com/MaelImhof/courseware-enhancer/issues/5
+         * 
+         * Pressing the space bar to play/pause the video scrolled the page
+         * down a bit, which can be prevented by the following event
+         * 
+         * When pressing the space bar out of full screen, the browser
+         * thinks that the space bar press was meant for the default target of the
+         * page, which results in a scrolling, so we prevent this default behavior
+         * 
+         * Stolen code from Stackoverflow
+         * https://stackoverflow.com/questions/22559830/html-prevent-space-bar-from-scrolling-page
+         */
+        window.addEventListener('keydown', (event) => {
+            // The default target is not the body on courseware.epfl.ch
+            if (event.key == CoursewareEnhancer.PauseResumeKey) {
+                event.preventDefault();
             }
         });
     }
